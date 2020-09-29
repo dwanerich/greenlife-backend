4e047436df2afcfb97d6b4bbd942cbbbc4a0ce32
dwanerich <dwanerich@gmail.com>
2020-09-28 20:47:35 -0400

    created backend API + generated User & Plant model

---
+++ db/migrate/20200929004607_create_plants.rb
@@ -0,0 +1,11 @@
+class CreatePlants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :plants do |t|
+      t.string :img_src
+      t.string :name
+      t.string :caption
+
+      t.timestamps
+    end
+  end
+end
